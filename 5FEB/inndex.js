f9c7a7d9f05dcfe1de1e4d98bc9f98e6f2901b14
prachidubey <prachidubey050@gmail.com>
2026-02-05 12:43:51 +0530

    vanshika

--- 5FEB/inndex.js
+++ 5FEB/inndex.js
@@ -4,3 +4,7 @@ const app = express();
 app.get('/',(req,res)=>{
     res.send("Root mei swagat hai")
 })
+const PORT = 8088;
+app.listen(PORT , ()=>{
+    console.log("SERVER CONNECTED AT PORT :${PORT}");
+})
